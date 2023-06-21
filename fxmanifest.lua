42bc3625ff31c0d7649ee83c27578ef86ae62a2f
Mattis "LayZ <59378110+sv-LayZ@users.noreply.github.com>
2023-06-21 05:13:59 +0200

    🚀 Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'common'
+
+author 'LayZ4Real'
+description 'UI Library used to be performant & easy to use'
+
+version '0.0.1'
+
+dependencies {
+    '/native:0x78608ACB'
+}
