1f018fb1dd0b3632ee424bfe235d3382581abd20
Mattis "LayZ <59378110+sv-LayZ@users.noreply.github.com>
2023-06-23 05:38:01 +0200

    🚀 adding description

--- web/src/components/Menu.vue
+++ web/src/components/Menu.vue
@@ -11,29 +11,39 @@ const menu = reactive({
   secondColor: "#F00",
   textColor: "#F0F",
   title: "Armurie",
+  description: "L4UI Lib is a performant and user-friendly library designed specifically for quickly creating user interfaces with CFX.RE API. This library allows you to develop interactive and visually appealing interfaces using common web languages such as HTML, CSS, and JavaScript.",
   banner: "@/assets/banner.png",
-  btn: [{ icon: "🚀", text: "Adder", suffix: "$500 000" }]
+  btn: [{ type: 1, icon: "🚀", text: "Adder", suffix: "$500 000" }]
 })
 
-console.log(app)
+for (let index = 0; index < 20; index++) {
+  menu.btn.push({ type: 1, icon: "🚀", text: "Adder", suffix: "$500 000" })
+}
+
 </script>
 
 <template>
   <div class="item">
-    <img src="@/assets/banner.png" class="banner">
-    <h1 class="title">{{ menu.title }}</h1>
-    <div class="buttons-list" v-for="item in menu.btn">
-      <div class="button">
+
+    <div class="header">
+      <img src="@/assets/banner.png" class="banner">
+      <h1 class="title">{{ menu.title }}</h1>
+    </div>
+    <p class="description" v-if="menu.description">{{ menu.description }}}</p>
+
+    <div class="buttons-list">
+      <div class="button" v-for="item in menu.btn">
         <div class="prefix">
-          <a class="icon">{{ item.icon }}</a>
+          <p class="icon" v-if="item.icon">{{ item.icon }}</p>
           <span>{{ item.text }}</span>
         </div>
         <div class="prefix">
           <span>{{ item.suffix }}</span>
-          <i v-if="item.type === 2">&lt;{{ item.index }}&gt;</i>
-          <i v-else>&gt;</i>
+          <p v-if="item.type === 2">&lt;{{ item.index }}&gt;</p>
+          <p v-else>&gt;</p>
         </div>
       </div>
     </div>
+
   </div>
 </template>
